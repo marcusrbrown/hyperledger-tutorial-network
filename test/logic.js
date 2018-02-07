1ca47475c3bccc82cbf740fd2cbd95d5227e2b0f
Marcus R. Brown <me@igetgam.es>
2018-02-07 09:54:29 -0700

    Add the tradeCommodity() transaction processor.

--- test/logic.js
+++ test/logic.js
@@ -17,110 +17,126 @@ const namespace = 'org.acme.biznet';
 const assetType = 'SampleAsset';
 
 describe('#' + namespace, () => {
-    // In-memory card store for testing so cards are not persisted to the file system
-    const cardStore = new MemoryCardStore();
-    let adminConnection;
-    let businessNetworkConnection;
+  // In-memory card store for testing so cards are not persisted to the file system
+  const cardStore = new MemoryCardStore();
+  let adminConnection;
+  let businessNetworkConnection;
 
-    before(() => {
-        // Embedded connection used for local testing
-        const connectionProfile = {
-            name: 'embedded',
-            type: 'embedded'
-        };
-        // Embedded connection does not need real credentials
-        const credentials = {
-            certificate: 'FAKE CERTIFICATE',
-            privateKey: 'FAKE PRIVATE KEY'
-        };
+  before(() => {
+    // Embedded connection used for local testing
+    const connectionProfile = {
+      name: 'embedded',
+      type: 'embedded'
+    };
+    // Embedded connection does not need real credentials
+    const credentials = {
+      certificate: 'FAKE CERTIFICATE',
+      privateKey: 'FAKE PRIVATE KEY'
+    };
 
-        // PeerAdmin identity used with the admin connection to deploy business networks
-        const deployerMetadata = {
-            version: 1,
-            userName: 'PeerAdmin',
-            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
-        };
-        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
-        deployerCard.setCredentials(credentials);
+    // PeerAdmin identity used with the admin connection to deploy business networks
+    const deployerMetadata = {
+      version: 1,
+      userName: 'PeerAdmin',
+      roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+    };
+    const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+    deployerCard.setCredentials(credentials);
 
-        const deployerCardName = 'PeerAdmin';
-        adminConnection = new AdminConnection({ cardStore: cardStore });
+    const deployerCardName = 'PeerAdmin';
+    adminConnection = new AdminConnection({ cardStore: cardStore });
 
-        return adminConnection.importCard(deployerCardName, deployerCard).then(() => {
-            return adminConnection.connect(deployerCardName);
-        });
+    return adminConnection.importCard(deployerCardName, deployerCard).then(() => {
+      return adminConnection.connect(deployerCardName);
     });
+  });
 
-    beforeEach(() => {
-        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+  beforeEach(() => {
+    businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
 
-        const adminUserName = 'admin';
-        let adminCardName;
-        let businessNetworkDefinition;
+    const adminUserName = 'admin';
+    let adminCardName;
+    let businessNetworkDefinition;
 
-        return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..')).then(definition => {
-            businessNetworkDefinition = definition;
-            // Install the Composer runtime for the new business network
-            return adminConnection.install(businessNetworkDefinition.getName());
-        }).then(() => {
-            // Start the business network and configure an network admin identity
-            const startOptions = {
-                networkAdmins: [
-                    {
-                        userName: adminUserName,
-                        enrollmentSecret: 'adminpw'
-                    }
-                ]
-            };
-            return adminConnection.start(businessNetworkDefinition, startOptions);
-        }).then(adminCards => {
-            // Import the network admin identity for us to use
-            adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`;
-            return adminConnection.importCard(adminCardName, adminCards.get(adminUserName));
-        }).then(() => {
-            // Connect to the business network using the network admin identity
-            return businessNetworkConnection.connect(adminCardName);
-        });
+    return BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..')).then(definition => {
+      businessNetworkDefinition = definition;
+      // Install the Composer runtime for the new business network
+      return adminConnection.install(businessNetworkDefinition.getName());
+    }).then(() => {
+      // Start the business network and configure an network admin identity
+      const startOptions = {
+        networkAdmins: [
+          {
+            userName: adminUserName,
+            enrollmentSecret: 'adminpw'
+          }
+        ]
+      };
+      return adminConnection.start(businessNetworkDefinition, startOptions);
+    }).then(adminCards => {
+      // Import the network admin identity for us to use
+      adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`;
+      return adminConnection.importCard(adminCardName, adminCards.get(adminUserName));
+    }).then(() => {
+      // Connect to the business network using the network admin identity
+      return businessNetworkConnection.connect(adminCardName);
     });
+  });
 
-    describe('ChangeAssetValue()', () => {
-        it('should change the value property of ' + assetType + ' to newValue', () => {
-            const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+  describe('#tradeCommodity', () => {
+    it('should be able to trade a commodity', () => {
+      const factory = businessNetworkConnection.getBusinessNetwork().getFactory();
 
-            // Create a user participant
-            const user = factory.newResource(namespace, 'User', 'Marcus R. Brown');
+      // Create the traders
+      const trader1 = factory.newResource(namespace, 'Trader', 'omcdonald@eieio.com');
+      const trader2 = factory.newResource(namespace, 'Trader', 'me@igetgam.es');
 
-            // Create the asset
-            const asset = factory.newResource(namespace, assetType, 'ASSET_001');
-            asset.value = 'old-value';
+      trader1.firstName = 'Old';
+      trader1.lastName = 'McDonald';
+      trader2.firstName = 'Marcus R.';
+      trader2.lastName = 'Brown';
 
-            // Create a transaction to change the asset's value property
-            const changeAssetValue = factory.newTransaction(namespace, 'ChangeAssetValue');
-            changeAssetValue.relatedAsset = factory.newRelationship(namespace, assetType, asset.$identifier);
-            changeAssetValue.newValue = 'new-value';
+      // Create the commodities
+      const commodity1 = factory.newResource(namespace, 'Commodity', 'EMA');
+      const commodity2 = factory.newResource(namespace, 'Commodity', 'XYZ');
 
-            let assetRegistry;
+      commodity1.description = 'Corn';
+      commodity1.mainExchange = 'Euronext';
+      commodity1.quantity = 100;
+      commodity1.owner = factory.newRelationship(namespace, 'Trader', trader1.$identifier);
+      commodity2.description = 'Soya';
+      commodity2.mainExchange = 'Chicago';
+      commodity2.quantity = 50;
+      commodity2.owner = factory.newRelationship(namespace, 'Trader', trader1.$identifier);
 
-            return businessNetworkConnection.getAssetRegistry(namespace + '.' + assetType).then(registry => {
-                assetRegistry = registry;
-                // Add the asset to the appropriate asset registry
-                return registry.add(asset);
-            }).then(() => {
-                return businessNetworkConnection.getParticipantRegistry(namespace + '.User');
-            }).then(userRegistry => {
-                // Add the user to the appropriate participant registry
-                return userRegistry.add(user);
-            }).then(() => {
-                // Submit the transaction
-                return businessNetworkConnection.submitTransaction(changeAssetValue);
-            }).then(registry => {
-                // Get the asset
-                return assetRegistry.get(asset.$identifier);
-            }).then(newAsset => {
-                // Assert that the asset has the new value property
-                newAsset.value.should.equal(changeAssetValue.newValue);
+      // Create the trade transaction
+      const trade = factory.newTransaction(namespace, 'Trade');
+
+      trade.newOwner = factory.newRelationship(namespace, 'Trader', trader2.$identifier);
+      trade.commodity = factory.newRelationship(namespace, 'Commodity', commodity1.$identifier);
+
+      return businessNetworkConnection.getAssetRegistry(namespace + '.Commodity')
+        .then((commodityRegistry) => {
+          return commodityRegistry.addAll([commodity1, commodity2])
+            .then(() => {
+              return businessNetworkConnection.getParticipantRegistry(namespace + '.Trader');
+            })
+            .then((participantRegistry) => {
+              return participantRegistry.addAll([trader1, trader2]);
+            })
+            .then(() => {
+              return businessNetworkConnection.submitTransaction(trade);
+            })
+            .then(() => {
+              return businessNetworkConnection.getAssetRegistry(namespace + '.Commodity');
+            })
+            .then ((commodityRegistry) => {
+              return commodityRegistry.get(commodity1.$identifier);
+            })
+            .then((commodity) => {
+              commodity.owner.$identifier.should.equal(trader2.$identifier);
             });
         });
     });
-
+  });
 });
